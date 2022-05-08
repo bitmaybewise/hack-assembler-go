27cfda66d48c8bc2f466ffbb9d44b8bc5e528d97
Hercules Merscher <hlmerscher@gmail.com>
2022-05-08 15:17:31 +0200

    main package... assembler without symbolic parser

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/hlmerscher/hack-assembler-go/assembler"
+)
+
+func main() {
+	var filename string
+	flag.StringVar(&filename, "f", "", "the filename of the asm source file")
+	flag.Parse()
+	if filename == "" {
+		panic("filename is missing")
+	}
+	fmt.Printf("file loaded: %s\n", filename)
+
+	asmFile := openAsmFile(filename)
+	defer asmFile.Close()
+	binary := assembler.Assemble(asmFile)
+	writeToHackFile(filename, binary)
+}
+
+func openAsmFile(filename string) *os.File {
+	inputFile, err := os.Open(filename)
+	if err != nil {
+		panic(fmt.Sprintf("error opening file <%s>\n", err))
+	}
+	return inputFile
+}
+
+func writeToHackFile(filename string, content string) {
+	outputFilename := strings.Replace(filename, ".asm", ".hack", 1)
+	fmt.Println("outputFilename:", outputFilename)
+
+	err := os.WriteFile(outputFilename, []byte(content), 0666)
+	if err != nil {
+		panic(err)
+	}
+}
